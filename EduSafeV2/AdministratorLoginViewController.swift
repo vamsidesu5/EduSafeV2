6ccceb31c8f02a5adf4cedd5a7e61e2d20215ad6
vamsidesu5 <vamsidesu5@gmail.com>
2018-02-24 22:01:24 -0500

    AY

---
+++ EduSafeV2/AdministratorLoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AdministratorLoginViewController.swift
+//  EduSafeV2
+//
+//  Created by vamsi on 2/24/18.
+//  Copyright © 2018 zav. All rights reserved.
+//
+
+import UIKit
+
+class AdministratorLoginViewController: UIViewController {
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        let user = userTextField.text;
+        let pass = passTextField.text;
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EduSafeV2/StudentSendReportViewController.swift
@@ -0,0 +1,55 @@
+//
+//  StudentSendReportViewController.swift
+//  EduSafeV2
+//
+//  Created by vamsi on 2/24/18.
+//  Copyright © 2018 zav. All rights reserved.
+//
+
+import UIKit
+
+class StudentSendReportViewController: UIViewController {
+    var urgency = 1;
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendAlertButtonPressed(_ sender: Any) {
+        let description = descriptionTextField.text;
+    }
+    
+    @IBAction func changedSegmentedControl(_ sender: Any)
+    {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            urgency = 1;
+        case 1:
+            urgency = 2;
+        case 2:
+            urgency = 3;
+        default:
+            break
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
