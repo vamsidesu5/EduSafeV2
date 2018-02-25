40b9cd25475995f4b0e0e91b8f0db62c371b0901
asong17 <andrewsong9999@gmail.com>
2018-02-24 23:05:35 -0500

    AF Functions

--- EduSafeV2/AFLogin.swift
+++ EduSafeV2/AFLogin.swift
@@ -9,6 +9,38 @@
 import Foundation
 import Alamofire
 
+struct AFMethods{
+
+    let endpoint = "hello" // Change this to the url
+
+    func loginStudent(username: String, password: String){
+
+
+    }
+
+    func loginAdmin(name: String, password: String){
+
+
+    }
+
+    func postMessage(content: String, urgency: Int){
+
+
+
+    }
+
+    func displayFeed(){
+
+
+
+    }
+
+    func approveMessage(username: String, password: String){
+        
+    }
+
+
+}
 
 
 
--- EduSafeV2/ViewController.swift
+++ EduSafeV2/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    static var endpoint: String = "Hello" // Put Url Here
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
